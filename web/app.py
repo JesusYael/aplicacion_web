115ed48e285831d82edf948e31468c113bf7e6d8
JesusYael <1721110698@utectulancingo.edu.mx>
2024-10-22 21:36:07 +0000

    First commit

--- web/app.py
+++ web/app.py
@@ -3,26 +3,24 @@ import requests
 
 urls = ("/", "index")
 app = web.application(urls, globals())
-render = web.template.render('./templates/')
+render = web.template.render('templates/')
 
 class index:
     def GET(self):
+        try:
+            api_url = 'https://api-rest-4-71ba.onrender.com/nombre'
+            response = requests.get(api_url)
 
-        api_url = 'https://api-rest-4-71ba.onrender.com/nombre'
-        # Realizar la petición GET a tu API local
-        response = requests.get(api_url)
+            if response.status_code == 200:
+                data = response.json()
+                nombre = data.get('nombre', 'desconocido')
+                return render.index(nombre)
+            else:
+                return f"Error al consumir la API: {response.status_code}"
+        except Exception as e:
+            return f"Error: {str(e)}"
 
-        # Verificamos que la respuesta sea exitosa (código 200)
-        if response.status_code == 200:
-            data = response.json()  # Obtenemos la respuesta en formato JSON
-            # Asumiendo que tu API retorna un JSON con clave 'mensaje' y 'valor'
-            nombre = data.get('nombre', 'desconocido')
-
-            # Pasamos los datos a la plantilla
-            return render.index(nombre)
-        else:
-            return "Error al consumir la API: " + str(response.status_code)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=int(os.environ.get("PORT", 8080)))
